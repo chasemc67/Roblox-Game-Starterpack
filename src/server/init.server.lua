1538b19ae45e5c8f00f4789f1da6a3e06d3ef216
chasemc67 <chasemc67@gmail.com>
2022-05-08 14:32:26 -0700

    Moved over ReplicatedStorage items

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,10 @@
-print("Hello world, from server!")
+-- Load all modules parented to this script and require them
+-- put them each in their own thread so if one is slow or errors ir
+-- doesn't impact the rest 
+for _, module in pairs(script:GetChildren()) do
+	local loadMod = coroutine.create(function()
+		require(module)
+	end)
+
+	coroutine.resume(loadMod)
+end
