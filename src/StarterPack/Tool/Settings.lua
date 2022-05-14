40c1b009ef1146bd970fe103fcbb4f59bfd42bc9
chasemc67 <chasemc67@gmail.com>
2022-05-14 13:22:42 -0700

    Split script files out of tool

---
+++ src/StarterPack/Tool/Settings.lua
@@ -0,0 +1,12 @@
+local gameSettings = {
+	fireMod = "SEMI";
+	damage = 15;
+	headshotMultiplier = 1.5;
+	rateOfFire = 300;
+	range = 500;
+	rayColor = Color3.fromRGB(255, 160, 75);
+	raySize = Vector2.new(0.25, 0.25);
+	debrisTime = 0.05;
+}
+
+return gameSettings
