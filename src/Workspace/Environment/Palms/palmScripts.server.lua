effc681a421855fd0c19bd67ce9f5465ae6a9d98
chasemc67 <chasemc67@gmail.com>
2022-05-14 21:52:52 -0700

    Proximity part is by the ground now

--- src/Workspace/Environment/Palms/palmScripts.server.lua
+++ src/Workspace/Environment/Palms/palmScripts.server.lua
@@ -12,7 +12,21 @@ for _, palm in pairs(palmFolder:GetChildren()) do
 		proximityPrompt.GamepadKeyCode = Enum.KeyCode.ButtonA
 		proximityPrompt.MaxActivationDistance = 500
 		proximityPrompt.HoldDuration = 3
-
-		proximityPrompt.Parent = palm
+		
+		
+		-- get the CFrame
+		-- get the length of the size 
+		-- get a new CFrame which is 3/4s of the length down the CFrame (ideally respecting position)
+		-- or, better yet, some number of units away from the end of the frame
+		local palmSize = palm.Size
+		print("Palm X: " .. palmSize.X .. "Palm Y: " .. palmSize.Y .. "Palm Z: " .. palmSize.Z)
+		local proximityPosition = CFrame.new(palm.Position - Vector3.new(0, palmSize.Y/2 - 5, 0))
+		
+		local promptPart = Instance.new("Part")
+		promptPart.Size = Vector3.new(0,0,0)
+		promptPart.Parent = palm
+		promptPart.CFrame = proximityPosition
+		promptPart.Anchored = true
+		proximityPrompt.Parent = promptPart
 	end
 end
