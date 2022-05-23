72143befb50dea389d90fd24a377495cdd038a6e
chasemc67 <chasemc67@gmail.com>
2022-05-22 17:45:37 -0700

    Added rodux test and Roact-rodux lib

---
+++ src/StarterPlayer/StarterPlayerScripts/Rodux_test.client.lua
@@ -0,0 +1,59 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Rodux = require(ReplicatedStorage.Rodux)
+
+-- Action creator for the ReceivedNewPhoneNumber action
+local function ReceivedNewPhoneNumber(phoneNumber)
+    return {
+        type = "ReceivedNewPhoneNumber",
+        phoneNumber = phoneNumber,
+    }
+end
+
+-- Action creator for the MadeNewFriends action
+local function MadeNewFriends(listOfNewFriends)
+    return {
+        type = "MadeNewFriends",
+        newFriends = listOfNewFriends,
+    }
+end
+
+-- Reducer for the current user's phone number
+local phoneNumberReducer = Rodux.createReducer("", {
+    ReceivedNewPhoneNumber = function(state, action)
+        return action.phoneNumber
+    end,
+})
+
+-- Reducer for the current user's list of friends
+local friendsReducer = Rodux.createReducer({}, {
+    MadeNewFriends = function(state, action)
+        local newState = {}
+
+        -- Since state is read-only, we copy it into newState
+        for index, friend in ipairs(state) do
+            newState[index] = friend
+        end
+
+        for _, friend in ipairs(action.newFriends) do
+            table.insert(newState, friend)
+        end
+
+        return newState
+    end,
+})
+
+local reducer = Rodux.combineReducers({
+    myPhoneNumber = phoneNumberReducer,
+    myFriends = friendsReducer,
+})
+
+local store = Rodux.Store.new(reducer, nil, {
+    Rodux.loggerMiddleware,
+})
+
+store:dispatch(ReceivedNewPhoneNumber("15552345678"))
+store:dispatch(MadeNewFriends({
+    "Cassandra",
+    "Joe",
+}))
