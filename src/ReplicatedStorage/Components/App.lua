89007025fe966b9c607fc17810de536e444d8b5c
chasemc67 <chasemc67@gmail.com>
2022-06-06 14:50:27 -0700

    Put back

---
+++ src/ReplicatedStorage/Components/App.lua
@@ -0,0 +1,47 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Roact = require(ReplicatedStorage.Roact)
+local RoactRodux = require(ReplicatedStorage.RoactRodux)
+
+local InventoryUIButton = require(ReplicatedStorage.Components.InventoryUIButton)
+local Inventory = require(ReplicatedStorage.Components.Inventory)
+
+local openInventory = require(ReplicatedStorage.Actions.openInventory)
+local increment = require(ReplicatedStorage.Actions.increment)
+
+local App = Roact.Component:extend("App")
+
+function App:init()
+end
+
+function App:render()
+    if (self.props.openWindow == "inventory") then
+        print("Rendering Inventory UI")
+        return Roact.createElement("ScreenGui", {}, {
+            Roact.createElement(Inventory)
+        })
+    end
+
+    return Roact.createElement("ScreenGui", {}, {
+        Roact.createElement(InventoryUIButton)
+    })
+end
+
+local function mapStateToProps(state)
+    return {
+        openWindow = state.ui.openWindow
+    }
+end
+
+local function mapDispatchToProps(dispatch)
+    return {
+        onIncrement = function()
+            dispatch(increment(1))
+        end,
+        onOpenInventory = function()
+            dispatch(openInventory())
+        end
+    }
+end
+
+return RoactRodux.connect(mapStateToProps, mapDispatchToProps)(App)
